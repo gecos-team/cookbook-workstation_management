b93a67fc5fe2d4be6867ccf01ed3c0c761a94a53
Roberto C. Morano <rcmorano@emergya.com>
2012-01-09 16:34:12 +0100

    added unique recipe to handle groups

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,7 +2,11 @@
 # Cookbook Name:: group_management
 # Recipe:: default
 #
-# Copyright 2011 damian
+# Copyright 2011 Junta de Andalucía
+#
+# Authors::
+#  * David Amian Valle <damian@emergya.com>
+#  * Roberto C. Morano <rcmorano@emergya.com>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,3 +20,21 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+require 'etc'
+
+node[:group_management].each do |grp_list,values|
+  node[:group_management][grp_list][:groups].each do |grp|
+    grp_members = Etc.getgrnam(grp).mem
+    grp_members = grp_members + node[:group_management][grp_list][:users_to_add]
+    unless grp_members.empty?
+      grp_members = grp_members - node[:group_management][grp_list][:users_to_remove]
+      grp_members.uniq!
+  
+      group grp do
+        action :manage
+        members grp_members
+        append false
+      end
+    end
+  end
+end
