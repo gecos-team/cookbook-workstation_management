2012b952a4080ef3280162636e4cf585446a5f29
Roberto C. Morano <rcmorano@emergya.com>
2012-02-08 14:00:48 +0100

    updated to support network_management recipe

--- metadata.rb
+++ metadata.rb
@@ -13,6 +13,10 @@ provides            "workstation_management::create_files"
 provides            "workstation_management::remove_files"
 recipe            "workstation_management::create_files", "Copy remote files to the Chef node."
 recipe            "workstation_management::remove_files", "Remove local files of the Chef node."
+
+provides            "workstation_management::network_management"
+recipe            "workstation_management::network_management", "Set network configuration"
+
 %w{ ubuntu debian }.each do |os|
   supports os
 end
@@ -94,7 +98,6 @@ attribute 'fixed_files/remove_files/file_path',
   :order        => "0",
   :recipes      => [ 'workstation_management::remove_files' ]
 
-
 attribute 'group_management/admin_users_to_add',
   :display_name => "Users to add to admin groups",
   :description  => "List of users that will be added to admin groups",
@@ -151,3 +154,77 @@ attribute 'group_management/base_users_to_remove/user',
   :validation   => "alphanumericwithdots",
   :order        => "3",
   :recipes      => ['workstation_management::group_management']
+
+attribute 'network_management/conn_type',
+  :display_name => "Connection type",
+  :description  => "Set connection type",
+  :type         => "string",
+  :choice       => [ "wired" ], #, "wireless" ],
+  :required     => "required",
+  :default      => "wired",
+  :order        => "0",
+  :recipes      => [ 'workstation_management::network_management' ]
+
+attribute 'network_management/dhcp',
+  :display_name => "DHCP",
+  :description  => "Set if network uses DHCP or not",
+  :type         => "string",
+  :choice       => [ "true", "false" ],
+  :required     => "required",
+  :default      => "true",
+  :order        => "1",
+  :recipes      => [ 'workstation_management::network_management' ]
+
+attribute 'network_management/ip_address',
+  :display_name => "IP",
+  :description  => "Set interface IP address",
+  :type         => "string",
+  :validation   => "ip",
+  :required     => "required",
+  :dependent    => [ { "field" => "network_management/dhcp", "validator" => "isfalse" } ],
+  :default      => "",
+  :order        => "2",
+  :recipes      => [ 'workstation_management::network_management' ]
+
+attribute 'network_management/gateway',
+  :display_name => "Gateway",
+  :description  => "Set interface gateway address",
+  :type         => "string",
+  :validation   => "ip",
+  :required     => "required",
+  :dependent    => [ { "field" => "network_management/dhcp", "validator" => "isfalse" } ],
+  :default      => "",
+  :order        => "3",
+  :recipes      => [ 'workstation_management::network_management' ]
+
+attribute 'network_management/netmask',
+  :display_name => "Netmask",
+  :description  => "Set interface netmask address",
+  :type         => "string",
+  :validation   => "ip",
+  :required     => "required",
+  :dependent    => [ { "field" => "network_management/dhcp", "validator" => "isfalse" } ],
+  :default      => "",
+  :order        => "4",
+  :recipes      => [ 'workstation_management::network_management' ]
+
+attribute 'network_management/dns_servers',
+  :display_name => "DNS Servers",
+  :description  => "",
+  :type         => "array",
+  :required     => "required",
+  :dependent    => [ { "field" => "network_management/dhcp", "validator" => "isfalse" } ],
+  :default      => "",
+  :recipes      => [ 'workstation_management::network_management' ]
+
+attribute 'network_management/dns_servers/ip',
+  :display_name => "DNS Server IP",
+  :description  => "DNS server's IPv4 address",
+  :type         => "string",
+  :validation   => "ip",
+  :required     => "required",
+  :dependent    => [ { "field" => "network_management/dhcp", "validator" => "isfalse" } ],
+  :default      => "",
+  :order        => "5",
+  :recipes      => [ 'workstation_management::network_management' ]
+
